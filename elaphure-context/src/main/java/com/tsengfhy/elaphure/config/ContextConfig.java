686c60ef906a20c86616e476ff9bfda3febc85e5
Tsengfhy <tsengfhy@gmail.com>
2021-08-20 19:08:28 +0800

    add cloud service general configuration

--- elaphure-context/src/main/java/com/tsengfhy/elaphure/config/ContextConfig.java
+++ elaphure-context/src/main/java/com/tsengfhy/elaphure/config/ContextConfig.java
@@ -1,7 +1,9 @@
 package com.tsengfhy.elaphure.config;
 
 import com.tsengfhy.elaphure.constants.Context;
+import com.tsengfhy.elaphure.env.ContextProperties;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -9,5 +11,6 @@ import org.springframework.core.Ordered;
 @Configuration(proxyBeanMethods = false)
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 @ComponentScan(basePackages = Context.BASE_PACKAGE)
+@EnableConfigurationProperties({ContextProperties.class})
 public class ContextConfig {
 }
